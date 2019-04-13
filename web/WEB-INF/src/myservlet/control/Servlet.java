774d0139f2f215720398e8735efc2cd0dcf924a7
ChengCuotuo <lei021191@outlook.com>
2019-04-13 22:51:16 +0800

    添加servlet 的注释

--- web/WEB-INF/src/myservlet/control/Servlet.java
+++ web/WEB-INF/src/myservlet/control/Servlet.java
@@ -22,6 +22,12 @@ public class Servlet extends HttpServlet {
     public void init(ServletConfig config)
         throws ServletException{
         super.init();
+        //加载数据库的驱动
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -29,6 +35,7 @@ public class Servlet extends HttpServlet {
         throws ServletException, IOException{
         request.setCharacterEncoding("utf-8");
         response.setContentType("text/html;charset=UTF-8");
+        //存储数据库的信息，并将它添加到 request 中
         Bean resultBean = null;
         try {
             resultBean = (Bean)request.getAttribute("resultBean");
@@ -42,16 +49,10 @@ public class Servlet extends HttpServlet {
             ex.printStackTrace();
         }
 
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
         //form 表单的 action 提交的内容，也就是 url 数据里面的内容
         String dataBase = request.getParameter("dataBase");
         String tableName = request.getParameter("tableName");
-
+        //由表单提交的信息
         String nu = request.getParameter("number");
         String na = request.getParameter("name");
         String mt = request.getParameter("madeTime");
@@ -60,14 +61,14 @@ public class Servlet extends HttpServlet {
         if (nu == null || nu.length() == 0) {
             fail(request, response, "添加数据失败，必须给出数据");
         }
-
+        //将提交的信息存储到数据库中
         float p = Float.parseFloat(pr);
         String codition = "insert into " + tableName + " values (" + "'" + nu + "', '"
                 + na +"', '" + mt + "', '" + pr + "')";
 
-        Connection conn;
-        Statement sql;
-        ResultSet rs;
+        Connection conn;    //建立连接
+        Statement sql;      //sql 语句
+        ResultSet rs;       //查询的结果集
 
         try {
             String url = "jdbc:mysql://127.0.0.1/" + dataBase
@@ -75,10 +76,11 @@ public class Servlet extends HttpServlet {
             conn = DriverManager.getConnection(url);
             sql = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
                     ResultSet.CONCUR_READ_ONLY);
-
+            //插入数据
             sql.executeUpdate(codition);
-
+            //查询数据
             rs = sql.executeQuery("select * from " + tableName);
+            //获取数据行数，列名
             ResultSetMetaData metaData = rs.getMetaData();
             int columnCount = metaData.getColumnCount();
             String[] columnName = new String[columnCount];
@@ -86,9 +88,12 @@ public class Servlet extends HttpServlet {
                 columnName[i] = metaData.getColumnName(i + 1);
             }
             resultBean.setColumnName(columnName);
-            rs.last();
 
+            //跳转到最后一列
+            rs.last();
+            //获取最后一列的列数
             int rowNumber = rs.getRow();
+            //将信息存储到 bean 中，需要先跳转到数据列最前面的前面，逐行获取数据
             String[][] tableRecord = new String[rowNumber][columnCount];
             rs.beforeFirst();
             int i = 0;
@@ -99,8 +104,9 @@ public class Servlet extends HttpServlet {
                 i++;
             }
             resultBean.setTableCord(tableRecord);
+            //关闭连接
             conn.close();
-
+            //重定向显示数据页面
             RequestDispatcher dispatcher = request.getRequestDispatcher("showRecord.jsp");
             dispatcher.forward(request, response);
         } catch (SQLException ex) {
@@ -114,7 +120,7 @@ public class Servlet extends HttpServlet {
         throws ServletException, IOException{
         doPost(request, response);
     }
-
+    //显示错误信息
     public void fail(HttpServletRequest request, HttpServletResponse response, String info){
         response.setContentType("text/html;charset=UTF-8");
         try {
